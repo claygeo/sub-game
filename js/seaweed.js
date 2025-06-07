66cf2f46d2f428b0c34ee110a60f673bb87a1a14
Clay <kmartclips@proton.me>
2025-06-06 21:29:35 -0400

    Delete js directory

--- js/seaweed.js
+++
@@ -1,7 +0,0 @@
-// Use global THREE
-export function createSeaweed(count) {
-    const geometry = new THREE.CylinderGeometry(0.1, 0.1, 5, 8);
-    const material = new THREE.MeshPhongMaterial({ color: 0x2e8b57 });
-    const seaweed = new THREE.InstancedMesh(geometry, material, count);
-    return seaweed;
-}
